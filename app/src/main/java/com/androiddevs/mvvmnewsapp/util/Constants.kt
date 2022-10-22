82c543e3c0f17fadae7f48495b5546f60b27d09b
stefany.forner <stefany.heringerforner@nomitri.com>
2022-10-22 15:09:05 +0200

    second commit

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -1,4 +1,10 @@
 package com.androiddevs.mvvmnewsapp.util
 
 class Constants {
+    companion object{
+        const val API_KEY = "162845d299ed4ce0ab139a29b697e2ef"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
 }
